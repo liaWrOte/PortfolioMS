2d97a71a6d22016fe10b8f096f4a7cd85f2832b0
liaWrOte <liaworkingonline@gmail.com>
2021-02-13 15:02:01 +0100

    devProjects responsive tablet

--- src/components/DevProjects/index.js
+++ src/components/DevProjects/index.js
@@ -29,97 +29,96 @@ const DevProjects = () => {
       <StyleRoot>
         <div className="devProjects">
 
-          <div>
-            <ul className="devProjects__module" style={styles.fadeIn}>
-              {/* -- DevProjects block */}
-              <li className="devProjects__module__square__devProjects">
-                <div className="devProjects__module__square__devProjects__block">
-                  <h2 className="devProjects__module__square__devProjects__block__mainTitle">
-                    <span className="devProjects__module__square__devProjects__block__mainTitle__small__title">Projets</span>
-                    <span className="devProjects__module__square__devProjects__block__mainTitle__big__title">Dev</span>
-                  </h2>
-                  <p className="devProjects__module__square__devProjects__block__description">Explorez mes projets</p>
-                </div>
-              </li>
+          <ul className="devProjects__module" style={styles.fadeIn}>
+            {/* -- DevProjects block */}
+            <li className="devProjects__module__square__devProjects">
+              <div className="devProjects__module__square__devProjects__block">
+                <h2 className="devProjects__module__square__devProjects__block__mainTitle">
+                  <span className="devProjects__module__square__devProjects__block__mainTitle__small__title">Projets</span>
+                  <span className="devProjects__module__square__devProjects__block__mainTitle__big__title">Dev</span>
+                </h2>
+                <p className="devProjects__module__square__devProjects__block__description">Explorez mes projets</p>
+              </div>
+            </li>
 
-              {/* -- Kikiveu block */}
+            {/* -- Kikiveu block */}
 
-              <li className="devProjects__module__square__kikiveu">
-                <Link to="/projets-dev/kikiveu">
-                  <div className="devProjects__module__square__kikiveu__block" />
-                  <div className="devProjects__module__square__kikiveu__content">
-                    <h2 className="devProjects__module__square__kikiveu__content__mainTitle">
-                      <div className="devProjects__module__square__kikiveu__content__mainTitle__small__title">
-                        <BigRoundedTag text="Kikiveu" />
-                      </div>
-                    </h2>
-                    <div className="devProjects__module__square__kikiveu__content__description">
-                      <SmallTag text="React/Redux/Axios" />
+            <li className="devProjects__module__square__kikiveu">
+              <Link to="/projets-dev/kikiveu">
+                <div className="devProjects__module__square__kikiveu__block" />
+                <div className="devProjects__module__square__kikiveu__content">
+                  <h2 className="devProjects__module__square__kikiveu__content__mainTitle">
+                    <div className="devProjects__module__square__kikiveu__content__mainTitle__small__title">
+                      <BigRoundedTag text="Kikiveu" />
                     </div>
-
-                  </div>
-                </Link>
-              </li>
-
-              {/* -- Portfolio 2021 */}
-              <li className="devProjects__module__square__portfolio2021">
-                <Link to="/projets-dev/portfolio2021">
-                  <div className="devProjects__module__square__portfolio2021__block" />
-                  <div className="devProjects__module__square__portfolio2021__content">
-                    <h2 className="devProjects__module__square__portfolio2021__content__mainTitle">
-                      <div className="devProjects__module__square__portfolio2021__content__mainTitle__small__title">
-                        <BigRoundedTag text="Portfolio 2021" />
-                      </div>
-                    </h2>
-                    <div className="devProjects__module__square__portfolio2021__content__description">
-                      <SmallTag text="React" />
-                    </div>
-
-                  </div>
-                </Link>
-              </li>
-
-              {/* -- Api block */}
-              <li className="devProjects__module__square__api">
-                <div className="devProjects__module__square__api__block">
-                  <h2 className="devProjects__module__square__api__block__mainTitle">
-                    <span className="devProjects__module__square__api__block__mainTitle__small__title">Git API</span>
                   </h2>
-                  <p className="devProjects__module__square__api__block__description">HTML/CSS/Javascript/Ajax</p>
-                </div>
-              </li>
-
-              {/* -- ToDoList block */}
-              <li className="devProjects__module__square__toDoList">
-                <div className="devProjects__module__square__toDoList__block">
-                  <h2 className="devProjects__module__square__toDoList__block__mainTitle">
-                    <span className="devProjects__module__square__toDoList__block__mainTitle__small__title">To do list</span>
-                  </h2>
-                  <p className="devProjects__module__square__toDoList__block__description">HTML/CSS/Javascript</p>
-                </div>
-              </li>
-
-              {/* -- Portfolio 2020 */}
-              <li className="devProjects__module__square__portfolio2020">
-                <Link to="/projets-dev/portfolio2020">
-                  <div className="devProjects__module__square__portfolio2020__block" />
-                  <div className="devProjects__module__square__portfolio2020__content">
-                    <h2 className="devProjects__module__square__portfolio2020__content__mainTitle">
-                      <div className="devProjects__module__square__portfolio2020__content__mainTitle__small__title">
-                        <BigRoundedTag text="Portfolio 2020" />
-                      </div>
-                    </h2>
-                    <div className="devProjects__module__square__portfolio2020__content__description">
-                      <SmallTag text="HTML/SCSS/Jquery" />
-                    </div>
-
+                  <div className="devProjects__module__square__kikiveu__content__description">
+                    <SmallTag text="React/Redux/Axios" />
                   </div>
-                </Link>
-              </li>
 
-            </ul>
-          </div>
+                </div>
+              </Link>
+            </li>
+
+            {/* -- Portfolio 2021 */}
+            <li className="devProjects__module__square__portfolio2021">
+              <Link to="/projets-dev/portfolio2021">
+                <div className="devProjects__module__square__portfolio2021__block" />
+                <div className="devProjects__module__square__portfolio2021__content">
+                  <h2 className="devProjects__module__square__portfolio2021__content__mainTitle">
+                    <div className="devProjects__module__square__portfolio2021__content__mainTitle__small__title">
+                      <BigRoundedTag text="Portfolio 2021" />
+                    </div>
+                  </h2>
+                  <div className="devProjects__module__square__portfolio2021__content__description">
+                    <SmallTag text="React" />
+                  </div>
+
+                </div>
+              </Link>
+            </li>
+
+            {/* -- Api block */}
+            <li className="devProjects__module__square__api">
+              <div className="devProjects__module__square__api__block">
+                <h2 className="devProjects__module__square__api__block__mainTitle">
+                  <span className="devProjects__module__square__api__block__mainTitle__small__title">Git API</span>
+                </h2>
+                <p className="devProjects__module__square__api__block__description">HTML/CSS/Javascript/Ajax</p>
+              </div>
+            </li>
+
+            {/* -- ToDoList block */}
+            <li className="devProjects__module__square__toDoList">
+              <div className="devProjects__module__square__toDoList__block">
+                <h2 className="devProjects__module__square__toDoList__block__mainTitle">
+                  <span className="devProjects__module__square__toDoList__block__mainTitle__small__title">To do list</span>
+                </h2>
+                <p className="devProjects__module__square__toDoList__block__description">HTML/CSS/Javascript</p>
+              </div>
+            </li>
+
+            {/* -- Portfolio 2020 */}
+            <li className="devProjects__module__square__portfolio2020">
+              <Link to="/projets-dev/portfolio2020">
+                <div className="devProjects__module__square__portfolio2020__block" />
+                <div className="devProjects__module__square__portfolio2020__content">
+                  <h2 className="devProjects__module__square__portfolio2020__content__mainTitle">
+                    <div className="devProjects__module__square__portfolio2020__content__mainTitle__small__title">
+                      <BigRoundedTag text="Portfolio 2020" />
+                    </div>
+                  </h2>
+                  <div className="devProjects__module__square__portfolio2020__content__description">
+                    <SmallTag text="HTML/SCSS/Jquery" />
+                  </div>
+
+                </div>
+              </Link>
+            </li>
+
+          </ul>
         </div>
+
       </StyleRoot>
     </>
   );
--- src/components/UxUiProjects/index.js
+++ src/components/UxUiProjects/index.js
@@ -28,96 +28,94 @@ const UxUiProjects = () => {
   return (
     <>
       <StyleRoot>
-        <div className="uxUiProjects" style={styles.fadeIn}>
+        <div className="uxUiProjects">
 
-          <div>
-            <ul className="uxUiProjects__module">
+          <ul className="uxUiProjects__module" style={styles.fadeIn}>
 
-              {/* -- uxUiProjects block */}
-              <li className="uxUiProjects__module__square__uxUiProjects">
-                <div className="uxUiProjects__module__square__uxUiProjects__block">
-                  <h2 className="uxUiProjects__module__square__uxUiProjects__block__mainTitle">
-                    <span className="uxUiProjects__module__square__uxUiProjects__block__mainTitle__small__title">Projets</span>
-                    <span className="uxUiProjects__module__square__uxUiProjects__block__mainTitle__big__title">UX/UI</span>
+            {/* -- uxUiProjects block */}
+            <li className="uxUiProjects__module__square__uxUiProjects">
+              <div className="uxUiProjects__module__square__uxUiProjects__block">
+                <h2 className="uxUiProjects__module__square__uxUiProjects__block__mainTitle">
+                  <span className="uxUiProjects__module__square__uxUiProjects__block__mainTitle__small__title">Projets</span>
+                  <span className="uxUiProjects__module__square__uxUiProjects__block__mainTitle__big__title">UX/UI</span>
+                </h2>
+                <p className="uxUiProjects__module__square__uxUiProjects__block__description">Explorez mes projets</p>
+              </div>
+            </li>
+
+            {/* -- Intégration wordpress */}
+            <li className="uxUiProjects__module__square__wordpress">
+              <Link to="/projets-uxui/wordpress">
+                <div className="uxUiProjects__module__square__wordpress__block" />
+                <div className="uxUiProjects__module__square__wordpress__content">
+                  <h2 className="uxUiProjects__module__square__wordpress__content__mainTitle">
+                    <div className="uxUiProjects__module__square__wordpress__content__mainTitle__small__title">
+                      <BigRoundedTag text="wordpress" />
+                    </div>
                   </h2>
-                  <p className="uxUiProjects__module__square__uxUiProjects__block__description">Explorez mes projets</p>
+                  <div className="uxUiProjects__module__square__wordpress__content__description">
+                    <SmallTag text="Intégration/webdesign" />
+                  </div>
+
                 </div>
-              </li>
+              </Link>
+            </li>
 
-              {/* -- Intégration wordpress */}
-              <li className="uxUiProjects__module__square__wordpress">
-                <Link to="/projets-uxui/wordpress">
-                  <div className="uxUiProjects__module__square__wordpress__block" />
-                  <div className="uxUiProjects__module__square__wordpress__content">
-                    <h2 className="uxUiProjects__module__square__wordpress__content__mainTitle">
-                      <div className="uxUiProjects__module__square__wordpress__content__mainTitle__small__title">
-                        <BigRoundedTag text="wordpress" />
-                      </div>
-                    </h2>
-                    <div className="uxUiProjects__module__square__wordpress__content__description">
-                      <SmallTag text="Intégration/webdesign" />
+            {/* -- IFPS */}
+            <li className="uxUiProjects__module__square__ifps">
+              <Link to="/projets-uxui/ifps">
+                <div className="uxUiProjects__module__square__ifps__block" />
+                <div className="uxUiProjects__module__square__ifps__content">
+                  <h2 className="uxUiProjects__module__square__ifps__content__mainTitle">
+                    <div className="uxUiProjects__module__square__ifps__content__mainTitle__small__title">
+                      <BigRoundedTag text="Webdesign" />
                     </div>
-
+                  </h2>
+                  <div className="uxUiProjects__module__square__ifps__content__description">
+                    <SmallTag text="Image de marque/Adobe XD" />
                   </div>
-                </Link>
-              </li>
 
-              {/* -- IFPS */}
-              <li className="uxUiProjects__module__square__ifps">
-                <Link to="/projets-uxui/ifps">
-                  <div className="uxUiProjects__module__square__ifps__block" />
-                  <div className="uxUiProjects__module__square__ifps__content">
-                    <h2 className="uxUiProjects__module__square__ifps__content__mainTitle">
-                      <div className="uxUiProjects__module__square__ifps__content__mainTitle__small__title">
-                        <BigRoundedTag text="Webdesign" />
-                      </div>
-                    </h2>
-                    <div className="uxUiProjects__module__square__ifps__content__description">
-                      <SmallTag text="Image de marque/Adobe XD" />
+                </div>
+              </Link>
+            </li>
+
+            {/* -- Illustration */}
+            <li className="uxUiProjects__module__square__illustration">
+              <Link to="/projets-uxui/illustration">
+                <div className="uxUiProjects__module__square__illustration__block" />
+                <div className="uxUiProjects__module__square__illustration__content">
+                  <h2 className="uxUiProjects__module__square__illustration__content__mainTitle">
+                    <div className="uxUiProjects__module__square__illustration__content__mainTitle__small__title">
+                      <BigRoundedTag text="illustration" />
                     </div>
-
+                  </h2>
+                  <div className="uxUiProjects__module__square__illustration__content__description">
+                    <SmallTag text="Adobe Illustrator" />
                   </div>
-                </Link>
-              </li>
 
-              {/* -- Illustration */}
-              <li className="uxUiProjects__module__square__illustration">
-                <Link to="/projets-uxui/illustration">
-                  <div className="uxUiProjects__module__square__illustration__block" />
-                  <div className="uxUiProjects__module__square__illustration__content">
-                    <h2 className="uxUiProjects__module__square__illustration__content__mainTitle">
-                      <div className="uxUiProjects__module__square__illustration__content__mainTitle__small__title">
-                        <BigRoundedTag text="illustration" />
-                      </div>
-                    </h2>
-                    <div className="uxUiProjects__module__square__illustration__content__description">
-                      <SmallTag text="Adobe Illustrator" />
+                </div>
+              </Link>
+            </li>
+
+            {/* -- Affiches */}
+            <li className="uxUiProjects__module__square__affiches">
+              <Link to="/projets-uxui/affiches">
+                <div className="uxUiProjects__module__square__affiches__block" />
+                <div className="uxUiProjects__module__square__affiches__content">
+                  <h2 className="uxUiProjects__module__square__affiches__content__mainTitle">
+                    <div className="uxUiProjects__module__square__affiches__content__mainTitle__small__title">
+                      <BigRoundedTag text="affiches" />
                     </div>
-
+                  </h2>
+                  <div className="uxUiProjects__module__square__affiches__content__description">
+                    <SmallTag text="Adobe Photoshop" />
                   </div>
-                </Link>
-              </li>
 
-              {/* -- Affiches */}
-              <li className="uxUiProjects__module__square__affiches">
-                <Link to="/projets-uxui/affiches">
-                  <div className="uxUiProjects__module__square__affiches__block" />
-                  <div className="uxUiProjects__module__square__affiches__content">
-                    <h2 className="uxUiProjects__module__square__affiches__content__mainTitle">
-                      <div className="uxUiProjects__module__square__affiches__content__mainTitle__small__title">
-                        <BigRoundedTag text="affiches" />
-                      </div>
-                    </h2>
-                    <div className="uxUiProjects__module__square__affiches__content__description">
-                      <SmallTag text="Adobe Photoshop" />
-                    </div>
+                </div>
+              </Link>
+            </li>
 
-                  </div>
-                </Link>
-              </li>
-
-            </ul>
-          </div>
+          </ul>
         </div>
       </StyleRoot>
     </>
